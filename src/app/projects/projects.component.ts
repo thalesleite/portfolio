b1468f3051221e3629bc3c275478caa65e490d20
thalesleite <thalesaleite@gmail.com>
2020-04-16 11:42:06 +0100

    changed domain from .com to .app for the netlifys link

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,7 +12,7 @@ export class ProjectsComponent implements OnInit {
     {
       name: 'orlanda',
       description: `It's a volunteer project for a nursing home with some advices of care for elders. The website was created using AngularJS 2 and SASS.`,
-      link: `https://orlanda.netlify.com`
+      link: `https://orlanda.netlify.app`
     },
     {
       name: 'monsters roledex',
@@ -25,21 +25,21 @@ export class ProjectsComponent implements OnInit {
       description: `It will help connect ongs with people that want to make the difference. 
                     They wanna be a hero. It is a fullstack app, backend in NodeJS, frontend in ReactJS and a 
                     mobile app using React Native.`,
-      link: `https://thales-be-the-hero.netlify.com`
+      link: `https://thales-be-the-hero.netlify.app`
     },
     {
       name: 'to do app',
       description: `With this web app you can put all your tasks that you wanna to complete.
                     It will help you to organise your day.
                     Technologies: Vanilla Javascript, HTML5 and CSS3`,
-      link: `https://thales-todo.netlify.com`
+      link: `https://thales-todo.netlify.app`
     },
     {
       name: 'notes app',
       description: `You can put your notes here in this web app, easily and faster.
                     It is a regular notes apps to help you to remember things.
                     Technologies: Vanilla Javascript, HTML5 and CSS3`,
-      link: `https://thales-notes.netlify.com`
+      link: `https://thales-notes.netlify.app`
     },
   ]
 
