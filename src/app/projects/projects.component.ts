0c96714dbcbd669fba505c49be6a032c7f1f329d
thalesleite <thalesaleite@gmail.com>
2020-04-16 11:37:16 +0100

    added two projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -16,13 +16,30 @@ export class ProjectsComponent implements OnInit {
     },
     {
       name: 'monsters roledex',
-      description: `This project is just an study of ReactJS and its basics. It is get some monsters data and you can filter it by name.`,
+      description: `This project is just an study of ReactJS and its basics. It is get some monsters 
+                    data and you can filter it by name.`,
       link: `https://thalesleite.github.io/monsters-roledex`
     },
     {
       name: 'be the hero',
-      description: `It is a project to help to connecto ongs with people that want to make the difference. They wanna be a hero. It is a fullstack app, backend in NodeJS, frontend in ReactJS and a mobile app using React Native.`,
+      description: `It will help connect ongs with people that want to make the difference. 
+                    They wanna be a hero. It is a fullstack app, backend in NodeJS, frontend in ReactJS and a 
+                    mobile app using React Native.`,
       link: `https://thales-be-the-hero.netlify.com`
+    },
+    {
+      name: 'to do app',
+      description: `With this web app you can put all your tasks that you wanna to complete.
+                    It will help you to organise your day.
+                    Technologies: Vanilla Javascript, HTML5 and CSS3`,
+      link: `https://thales-todo.netlify.com`
+    },
+    {
+      name: 'notes app',
+      description: `You can put your notes here in this web app, easily and faster.
+                    It is a regular notes apps to help you to remember things.
+                    Technologies: Vanilla Javascript, HTML5 and CSS3`,
+      link: `https://thales-notes.netlify.com`
     },
   ]
 
